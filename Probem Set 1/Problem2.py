59f8e66512d0044b34626f16bc3dc9a6892c2e65
gnimelff <31558289+gnimelff@users.noreply.github.com>
2019-08-28 18:09:02 +0000

    Update Problem2.py

--- Probem Set 1/Problem2.py
+++ Probem Set 1/Problem2.py
@@ -1,13 +1,10 @@
 #Assume s is a string of lower case characters.
 #Write a program that prints the number of times the string 'bob' occurs in s.
 
-count = 0                   
-start = 0                   
-for letter in range(len(s[:-2])):
-    if s[start:(start+3)] == "bob":
-        count += 1
-        start += 1
-    else: 
-        start += 1
+bobCount = 0
 
-print("Number of times bob occurs is: " + str(count))
+for i in range(len(s)):
+  if s[i:i+3] == 'bob':
+    bobCount += 1
+
+print('Number of times bob occurs is : ' + str(bobCount))
